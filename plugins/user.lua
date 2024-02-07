39ebdadcec3f37aa954fab7e4be03a14da8460c3
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2024-02-08 00:46:39 +0100

    Added plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -8,5 +8,42 @@ return {
   --   config = function()
   --     require("lsp_signature").setup()
   --   end,
-  -- },
+  -- },:
+  {
+    "echasnovski/mini.move",
+    lazy = false,
+    version = false,
+    config = function()
+      require("mini.move").setup {
+        mappings = {
+          -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = "<leader>mh",
+          right = "<leader>ml",
+          down = "<leader>mj",
+          up = "<leader>mk",
+
+          -- Move current line in Normal mode
+          line_left = "<leader>mh",
+          line_right = "<leader>ml",
+          line_down = "<leader>mj",
+          line_up = "<leader>mk",
+        },
+      }
+    end,
+  },
+  {
+    "ThePrimeagen/vim-be-good",
+    lazy = false,
+  },
+  {
+    "stevearc/oil.nvim",
+    --opts = {},
+    -- Optional dependencies
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+    end,
+  },
 }
